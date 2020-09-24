5d7d1f96dc9751694b4c548a617cb3d51fb66696
Josiah Fowler <josiah.james.fowler@gmail.com>
2020-09-24 09:45:41 -0400

    another one

--- numberOfGoodPairs.js
+++ numberOfGoodPairs.js
@@ -1,3 +1,7 @@
+//nums = array of integers
+// pair (i,j) is called good IF nms[i] == nums[j] AND i < j
+// Return the number of good pairs
+
 const numIdenticalPairs = (nums) => {
   const map = new Map();
   let count = 0;
@@ -13,3 +17,4 @@ const numIdenticalPairs = (nums) => {
 };
 
 // Testcase [1,2,3,1,1,3]
+// Runtime: 76 ms
---
+++ shuffleTheArray.js
@@ -0,0 +1,22 @@
+// array of nums - (2n) elements [x1,x2,...,xn,y1,y2,...,yn]
+// return array in form of [x1,y1,x2,y2,...,xn,yn]
+
+// Example:
+// Input: nums = [2,5,1,3,4,7], n = 3
+// Output: [2,3,5,4,1,7]
+// Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+
+const shuffle = (nums, n) => {
+  const results = [];
+  let a = n;
+  for (let i = 0; i < n; i++) {
+    results.push(nums[i]);
+    results.push(nums[a]);
+    a++;
+  }
+  return results;
+};
+
+// Testcase:
+// [2,5,1,3,4,7]
+// 3
